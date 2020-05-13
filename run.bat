83ed95d1cd5b92e98513797619f7a4446ec4d35d
Rishav Bhagat <rishavb123@gmail.com>
2020-05-12 20:49:07 -0400

    updated build script

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
 @echo off
 dir /s /B *.java > sources.txt
+if not exist build\* (
+    mkdir build
+)
 javac -cp ".;./lib/*" -d build  @sources.txt
 java -cp "build;./lib/*" io.bhagat.paint.PaintProgram
